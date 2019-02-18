bef894e1d5619c4a26d5c546f0d50424d6d256d6
NFLorD <46872160+NFLorD@users.noreply.github.com>
2019-02-18 20:37:43 +0100

    Add files via upload

--- carnet.js
+++ carnet.js
@@ -1,114 +1,121 @@
 "use_strict";
-
-// HIDE WHATS SUPPOSED TO BE HIDDEN
-$("#form").hide();
-$("#card").hide();
-
-// SET EDIT CHECKER FALSE BY DEFAULT
-let isEdit = false;
-
-// OUR LIST OF ENTRIES
-let entries = [
-  {
-    firstName: "Nicolas",
-    lastName: "Fasano",
-    phone: "0623309121",
-    civility: "M."
-  },
-  {
-    firstName: "Elise",
-    lastName: "Fasano",
-    phone: "0123456789",
-    civility: "Mme"
-  }
-];
-
-function display() {
-  $("#list").html("");
-  entries.forEach(function(entry, index) {
-    $("#list").append(
-      "<li><a href='#' data-id='" +
-        index +
-        "'>" +
-        entry.firstName +
-        " " +
-        entry.lastName.toUpperCase() +
-        "</a></li>"
-    );
-  });
-}
-
-function cardDisplay(a) {
-  let obj = entries[a[0].dataset.id];
-  $("#card h3").text(obj.civility + " " + obj.firstName + " " + obj.lastName);
-  $("#card em").text(obj.phone);
-  $("#card a").attr("data-id", a[0].dataset.id);
-}
-
-function showForm() {
-  $("#form").toggle();
-  isEdit = false;
-  $("#add").text("Ajouter");
-  $("#lastName").val("");
-  $("#firstName").val("");
-  $("#phone").val("");
-  $("#civility").val("M.");
-}
-
-function add() {
-  entries.push({
-    firstName: $("#firstName").val(),
-    lastName: $("#lastName").val(),
-    phone: $("#phone").val(),
-    civility: $("#civility").val()
-  });
-  display();
-}
-
-function edit() {
-  let obj = entries[$(this)[0].dataset.id];
-  obj.firstName = $("#firstName").val();
-  obj.lastName = $("#lastName").val();
-  obj.phone = $("#phone").val();
-  obj.civility = $("#civility").val();
-  showForm();
-  $("#card").hide();
-}
-
-// START PROGRAM
 $(function() {
+
+  // HIDE WHATS SUPPOSED TO BE HIDDEN
+  $("#form").hide();
+  $("#card").hide();
+
+  // SET EDIT CHECKER FALSE BY DEFAULT
+  let isEdit = false;
+
+  // OUR LIST OF ENTRIES
+  if(localStorage.getItem('entries') !== null){
+    var entries = JSON.parse(localStorage.getItem('entries'));
+  } else {
+    var entries = [];
+  }
+
+  // DISPLAY (CALLED BY ADD/EDIT)
+  function display() {
+    $("#list").html("");
+    entries.forEach(function(entry, index) {
+      $("#list").append(
+        "<li><a href='#' data-id='" +
+          index +
+          "'>" +
+          entry.firstName +
+          " " +
+          entry.lastName.toUpperCase() +
+          "</a></li>"
+      );  
+    });
+  }
+
+  // CARD (CALLED BY EDIT/CLICK ON A LIST LINK)
+  function cardDisplay(id) {
+    let obj = entries[id];
+    $("#card h3").text(obj.civility + " " + obj.firstName + " " + obj.lastName);
+    $("#card em").text(obj.phone);
+    $("#card a").attr("data-id", id);
+  }
+
+  // TOGGLED BY BUTTON CLICK (#show) OR EDITING
+  function showForm() {
+    $("#form").toggle();
+    isEdit = false;
+    $("#add").text("Ajouter");
+    $("#lastName").val("");
+    $("#firstName").val("");
+    $("#phone").val("");
+    $("#civility").val("M.");
+  }
+
+  // TOGGLED BY BUTTON CLICK (#add) WHEN EDIT == FALSE
+  function add() {
+    entries.push({
+      firstName: $("#firstName").val(),
+      lastName: $("#lastName").val(),
+      phone: $("#phone").val(),
+      civility: $("#civility").val()
+    });
+    display();
+  }
+
+  // TOGGLE BY BUTTON CLICK (#add) WHEN EDIT == true, SEES ITSELF PASSED THE INDEX TO CHANGE
+  function edit(id) {
+    let obj = entries[id];
+    obj.firstName = $("#firstName").val();
+    obj.lastName = $("#lastName").val();
+    obj.phone = $("#phone").val();
+    obj.civility = $("#civility").val();
+    showForm();
+    cardDisplay(id);
+    display();
+  }
+
+  // START PROGRAM
   display();
 
-  // ONCLICK FORM SHOWS
+  // BIND FORM TO BTN
   $("#show").click(showForm);
 
-  // CLEARS THE LIST AND HIDES THE CARD
+  // CLEARS THE LIST AND HIDES THE CARD, ENTRIES IS NOW EMPTY/NULL IN LOCALSTORAGE
   $("#del").click(function() {
     $("#list").html("");
     $("#card").hide();
     entries = [];
+    localStorage.removeItem('entries');
   });
 
-  // ONCLICK ADD
+  // BIND LIST UPDATES TO BTN
   $("#add").click(function(e) {
     e.preventDefault();
-    if (isEdit !== "") {
-      edit();
+    if (isEdit == true) {
+      edit($(this)[0].dataset.id);
     } else {
       add();
     }
+    // EVERY UPDATE ENDS WITH A LOCALSTORAGE VARIABLE UPDATE
+    localStorage.setItem('entries', JSON.stringify(entries));
   });
 
-  // ONCLICK SHOW CARD AND FILL IT WITH RIGHT INFO
+  // BIND LIST LINK WITH DETAILS CARD
   $("#list").on("click", "li>a", function() {
-    cardDisplay($(this));
+    cardDisplay($(this)[0].dataset.id);
     $("#card").show();
   });
 
-  // ONCLICK SHOW FORM AND FILL IT WITH RIGHT INFO, SET BTN TO 'MODIFIER' AND EDIT TO TRUE
+  // BIND CARD LINK TO SHOW FORM AND FILL IT WITH RIGHT INFO, SET BTN TO 'MODIFIER' AND EDIT TO TRUE
   $("#card").on("click", "a", function() {
-    $("#form").show();
     isEdit = true;
+    let obj = entries[$(this)[0].dataset.id];
     $("#add").text("Modifier");
+    $("#add").attr("data-id", $(this)[0].dataset.id);
+    $("#lastName").val(obj.lastName);
+    $("#firstName").val(obj.firstName);
+    $("#phone").val(obj.phone);
+    $("#civility").val(obj.civility);
+    $("#form").show();
   });
+
 });
